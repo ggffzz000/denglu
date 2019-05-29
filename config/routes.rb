5177609696e8944d423a8b3fca752132dfe31bd3
ggffzz000 <714576549@qq.com>
2019-05-28 18:47:40 -0700

    nice

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get '/help', to:'static_pages#help'
+
+  get '/signup', to: 'users#new'
+
+  get 'static_pages/about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+end
